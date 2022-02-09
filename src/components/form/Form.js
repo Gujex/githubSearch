3e799e108a45a78febbced780da636659a661224
Gujex <txere@bk.ru>
2022-02-09 20:09:02 +0400

    changed some style

--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,38 +1,47 @@
-import React from 'react';
-import styles from './form.module.css'
+import React from "react";
+import styles from "./form.module.css";
+import { FaSearch } from "react-icons/fa";
 
 function Form(props) {
-  return <form className={styles.wrapper}>
-  <div className={styles.search_div}>
-    <input
-      onChange={(e) => props.onFetchData(e.target.value)}
-      className={styles.formInput}
-      type="text"
-      placeholder="Search user..
+  return (
+    <form className={styles.wrapper}>
+      <div className={styles.search_div}>
+        <div className={styles.inputParent}>
+          <FaSearch className={styles.searchIcon} />
+          <input
+            onChange={(e) => props.onFetchData(e.target.value)}
+            className={styles.formInput}
+            type="text"
+            placeholder="Search user..
     "
-    />
-    {props.users.length === 0 || props.users[0].message
-      ? ""
-      : props.users[0].items.slice(0, 20).map((item, index) => {
-          const { login, avatar_url } = item;
-          return (
-            <div key={index} className={styles.usersList}>
-              <a
-                className={styles.linkContainer}
-                key={index}
-                href={`https://github.com/${login}`}
-                target="_blank"
-              >
-                <div>
-                  <img src={avatar_url} alt="" />
-                </div>
-                <div style={{ marginLeft: "10px" }}>{login}</div>
-              </a>
-            </div>
-          );
-        })}
-  </div>
-</form>
+          />
+        </div>
+        <div className={styles.itemsDiv}>
+          {props.users.length === 0 || props.users[0].message
+            ? ""
+            : props.users[0].items.slice(0, 20).map((item, index) => {
+                const { login, avatar_url } = item;
+                return (
+                  <div key={index} className={styles.usersList}>
+                    <a
+                      className={styles.linkContainer}
+                      key={index}
+                      href={`https://github.com/${login}`}
+                      target="_blank"
+                    >
+                      <div>
+                        <img src={avatar_url} alt="" />
+                      </div>
+                      <div style={{ marginLeft: "10px" }}>{login}</div>
+                    </a>
+                  </div>
+                );
+              })}
+        </div>
+      </div>
+      {props.showAlert && <p>{props.apiMessage}</p>}
+    </form>
+  );
 }
 
 export default Form;
--- src/components/searchBar/SearchBar.js
+++ src/components/searchBar/SearchBar.js
@@ -1,15 +1,28 @@
 import React, { useState, useEffect } from "react";
 import Form from "../form/Form";
 
+const API_ERROR =
+  "API rate limit exceeded for 185.70.52.89. (But here's the good news: Authenticated requests get a higher rate limit. Check out the documentation for more details.)";
 function SearchBar() {
-  const [userName, setUserName] = useState("");
+  // const [userName, setUserName] = useState("");
   const [users, setUsers] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
+  const [apiMessage, setApimessage] = useState(false);
 
   const fetchData = async (user) => {
     const URL = `https://api.github.com/search/users?q=${user}`;
 
     try {
       let response = await fetch(URL);
+      if (response.status === 403) {
+        setShowAlert(true);
+        setApimessage(
+          "API rate limit exceeded, please wait for a while and try again "
+        );
+      } else {
+        setShowAlert(false);
+      }
+
       let data = await response.json();
       setUsers([data]);
     } catch (error) {
@@ -19,7 +32,12 @@ function SearchBar() {
 
   return (
     <>
-      <Form users={users} onFetchData={fetchData} />
+      <Form
+        showAlert={showAlert}
+        apiMessage={apiMessage}
+        users={users}
+        onFetchData={fetchData}
+      />
     </>
   );
 }
